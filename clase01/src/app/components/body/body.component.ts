461e8c2959eefcb588ed855c5e2b550dac4e656d
amloarte <axel_manuel@outlook.es>
2020-04-21 10:23:56 -0600

    Clase 02

---
+++ clase01/src/app/components/body/body.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+
+export class BodyComponent {
+  mostrar = true;
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker',
+  };
+
+  personajes: string[] = ['Spaiderman', 'Benom', 'Capitan America'];
+
+}
